9e8bad5f7cce580b300d4e7823e2ac93aaf6b833
bansalpratham <prathambansal200@gmail.com>
2026-09-11 11:06:22 +0530

    First Commit

---
+++ src/main/java/com/example/ucs067/studentmanagement/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.ucs067.studentmanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Spring Boot!";
+    }
+
+}
